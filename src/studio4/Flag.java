6659c7b0e452afde9060e0afaf1b4a220bda4e16
Tanuja Gunapooti <Tanuja Gunapooti@DESKTOP-9E85GLS.dhcp.wustl.edu>
2022-10-13 14:00:15 -0500

    Finished

--- src/studio4/Flag.java
+++ src/studio4/Flag.java
@@ -6,6 +6,33 @@ import edu.princeton.cs.introcs.StdDraw;
 
 public class Flag {
 	public static void main(String[] args) {
+		// makes background green on flag
+		Color Trent = new Color (131,193,80); //new green color
+		StdDraw.clear(Trent); //setting background to green
+		
+		//Creating pig outline
+		StdDraw.setPenRadius(.005); // for the outline but doesn't matter because it's filled
+		Color Becca = new Color (255,156,208); // pink color of pig
+		StdDraw.setPenColor(Becca);
+		//Circle ears
+		StdDraw.filledCircle(.3, .79, .1);
+		StdDraw.filledCircle(.7, .79, .1);
+		//circle face
+		StdDraw.filledCircle(.5, .5, .25);
+		//Black pen color 
+		StdDraw.setPenColor(Color.BLACK);
+		//outlining face + ears
+		StdDraw.circle(.3, .79, .1);
+		StdDraw.circle(.7, .79, .1);
+		StdDraw.circle(.5, .5, .25);
+		//nose
+		StdDraw.ellipse(.5, .45, .12, .08);
+		//nostril
+		StdDraw.circle(.48, .45, .01);
+		StdDraw.circle(.52, .45, .01);
+		//eyes
+		StdDraw.filledCircle(.44, .6, .017);
+		StdDraw.filledCircle(.56, .6, .017);
 		
 	}
 }
--- src/studio4/InterpretDrawingFile.java
+++ src/studio4/InterpretDrawingFile.java
@@ -1,5 +1,6 @@
 package studio4;
 
+
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.Scanner;
@@ -19,6 +20,40 @@ public class InterpretDrawingFile {
 		chooser.showOpenDialog(null);
 		File f = new File(chooser.getSelectedFile().getPath());
 		Scanner in = new Scanner(f); //making Scanner with a File
+		
+		String shapeType = in.next(); //reads one word no matter what it is
+		int red = in.nextInt();
+		int green = in.nextInt();
+		int blue = in.nextInt();
+		
+		boolean isFilled = in.nextBoolean();
+		
+		//points of rectangle from resources documents
+		double parameterOne = in.nextDouble();
+		double parameterTwo = in.nextDouble();
+		double parameterThree = in.nextDouble();
+		double parameterFour = in.nextDouble();
+		double parameterFive = in.nextDouble();
+		double parameterSix = in.nextDouble();
+		
+		//get color
+		StdDraw.setPenColor(red, green, blue);
+		
+		//rectangle is outline or filled in
+		if (isFilled == true) {
+			StdDraw.filledRectangle(parameterOne, parameterTwo, parameterThree, parameterFour);
+		}
+		else {
+			StdDraw.rectangle(parameterOne, parameterTwo, parameterThree, parameterFour);
+		}
+		
+		//generalize code
+		if(shapeType.equals ("ellipse")) {
+			StdDraw.ellipse(parameterOne, parameterTwo, parameterThree, parameterFour);
+		}
+		else if (shapeType.equals ("triangle")) {
+			StdDraw.polygon(null, null);; // idk if this works probably not
+		}
 		
 	}
 }
